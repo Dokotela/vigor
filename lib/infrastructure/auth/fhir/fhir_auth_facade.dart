683420fb3b66abbaa7c5bfc97a5992ed11ec75e5
Dokotela <jgfaulkenberry@gmail.com>
2020-07-31 16:04:03 -0400

    I'm tired today. I'll think on this later.

--- lib/infrastructure/auth/fhir/fhir_auth_facade.dart
+++ lib/infrastructure/auth/fhir/fhir_auth_facade.dart
@@ -1,8 +1,8 @@
-import 'package:vigor/domain/auth/i_auth_facade.dart';
+// import 'package:vigor/domain/auth/i_auth_facade.dart';
 
-class FhirAuthFacade implements IAuthFacade {
-  final SmartAuth _smartAuth;
-  final GenericAuth _genericAuth;
+// class FhirAuthFacade implements IAuthFacade {
+//   final SmartAuth _smartAuth;
+//   final GenericAuth _genericAuth;
 
-  FhirAuthFacade(this._smartAuth, this._genericAuth);
-}
+//   FhirAuthFacade(this._smartAuth, this._genericAuth);
+// }
---
+++ lib/infrastructure/auth/smart/smart_client.dart
@@ -0,0 +1,5 @@
+import 'package:fhir/fhir_r4.dart';
+
+class SmartClient {
+  final FhirUri fhirBaseUrl = '';
+}
