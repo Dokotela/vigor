3a3eedc46a79c68e8f1484dd1d2ec98e8d6cb26b
John Manning <john.d.manning@gmail.com>
2020-09-29 14:14:51 -0400

    feat: minor layout changes for home screen

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -12,75 +12,83 @@ class HomeScreen extends StatelessWidget {
       init: HomeController(),
       builder: (controller) => Scaffold(
         appBar: AppBar(title: const Text('VaxCast')),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              runAlignment: WrapAlignment.spaceBetween,
-              direction: Axis.horizontal,
-              children: <Widget>[
-                Text(
-                  'VaxCast',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.courgette(
-                      textStyle:
-                          const TextStyle(color: Colors.red, fontSize: 60)),
-                ),
-                Text(
-                  'Powered by FHIRFLI',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.averiaLibre(
-                      textStyle:
-                          const TextStyle(color: Colors.black, fontSize: 40)),
-                ),
-                DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                      items: controller.nameList.map((String name) {
-                        return DropdownMenuItem<String>(
-                          value: name,
-                          child: Text(name),
-                        );
-                      }).toList(),
-                      hint: Text(
-                        "Select Patient's Name".tr,
-                      ),
-                      onChanged: (newName) =>
-                          controller.event(HomeEvent.choosePatient(newName))),
-                ),
-                Text(
-                  controller.nameError.tr,
-                  style: const TextStyle(fontSize: 12.0, color: Colors.red),
-                ),
-                TextFormField(
-                  controller: controller.nameController,
-                  decoration: InputDecoration(
-                    hintText: 'Enter New Patient Name'.tr,
-                    errorText: controller.newNameError.tr,
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                children: <Widget>[
+                  _spacer(),
+                  Center(
+                    child: Text(
+                      'VaxCast',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.courgette(
+                          textStyle:
+                              const TextStyle(color: Colors.red, fontSize: 60)),
+                    ),
                   ),
-                ),
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
+                  Center(
+                    child: Text(
+                      'Powered by FHIRFLI',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.averiaLibre(
+                          textStyle: const TextStyle(
+                              color: Colors.black, fontSize: 40)),
+                    ),
                   ),
-                  onPressed: () => controller.event(
-                    HomeEvent.registerPatient(controller.nameController.text),
+                  _spacer(),
+                  DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                        items: controller.nameList.map((String name) {
+                          return DropdownMenuItem<String>(
+                            value: name,
+                            child: Text(name),
+                          );
+                        }).toList(),
+                        hint: Text(
+                          "Select Patient's Name".tr,
+                        ),
+                        onChanged: (newName) =>
+                            controller.event(HomeEvent.choosePatient(newName))),
                   ),
-                  child: Text('Click to Register New Patient'.tr),
-                ),
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
+                  Text(
+                    controller.nameError.tr,
+                    style: const TextStyle(fontSize: 12.0, color: Colors.red),
                   ),
-                  onPressed: () async =>
-                      await ResourceDao().deleteAllResources(),
-                ),
-              ],
+                  _spacer(),
+                  TextFormField(
+                    controller: controller.nameController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter New Patient Name'.tr,
+                      errorText: controller.newNameError.tr,
+                    ),
+                  ),
+                  _spacer(),
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    onPressed: () => controller.event(
+                      HomeEvent.registerPatient(controller.nameController.text),
+                    ),
+                    child: Text('Click to Register New Patient'.tr),
+                  ),
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    onPressed: () async =>
+                        await ResourceDao().deleteAllResources(),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+
+  Widget _spacer() => const SizedBox(height: 24.0);
 }
