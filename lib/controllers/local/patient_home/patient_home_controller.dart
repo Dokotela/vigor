1527d0d85cb72f4da1b10d5ebb3b9b5ba4424164
Dokotela <jgfaulkenberry@gmail.com>
2020-11-23 16:12:55 -0500

    removed tabs from homepage

--- lib/controllers/local/patient_home/patient_home_controller.dart
+++ lib/controllers/local/patient_home/patient_home_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vigor/routes/routes.dart';
 import '../../../models/data/patient_model.dart';
@@ -10,26 +9,6 @@ class PatientHomeController extends GetxController
   /// PROPERTIES
   final patient = PatientModel(patient: Get.arguments);
   final RxInt _tabIndex = 0.obs;
-  final List<String> _tabsList = [
-    'Vaccines',
-    'Growth\nCurve',
-    'Milestones',
-  ]; // todo: extract list, add internationalization
-  TabController _tabController;
-
-  /// INIT
-  @override
-  void onInit() {
-    _tabController = TabController(length: _tabsList.length, vsync: this);
-    super.onInit();
-  }
-
-  /// CLOSE
-  @override
-  void onClose() {
-    _tabController.dispose();
-    super.onClose();
-  }
 
   /// SETTER FUNCTIONS
   set tabIndex(int value) => _tabIndex.value = value;
@@ -42,8 +21,6 @@ class PatientHomeController extends GetxController
   String relativeAge() => sharedRelativeAge(patient.birthDate());
 
   int get tabIndex => _tabIndex.value;
-  List<String> tabsList() => _tabsList;
-  TabController get tabController => _tabController;
 
   /// EVENTS
   void editPatient() =>
